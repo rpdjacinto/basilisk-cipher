8ca59a892e0f8d43b14cfe0bf67552f714c1c873
Rafael Jacinto <rpdjacinto@gmail.com>
2020-11-11 21:45:38 -0800

    Added ciper app

---
+++ src/components/app/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Background from '../../components/background'
+import Cipher from '../../components/cipher'
+
+const App = () => {
+    return (<Background>
+        <Cipher />
+    </Background>)
+}
+
+export default App
---
+++ src/components/background/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Background = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(135deg, #415a77, #0d1b2a);
+    display: grid;
+    place-items: center;
+`
+
+export default Background
